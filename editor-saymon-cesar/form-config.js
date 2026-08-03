1fbd005ce1c2acd2034fac3fe0ed94a7303fa839
fantasticoseditora <fantasticoseditora@gmail.com>
2026-08-03 19:08:12 -0300

    Prepara configuração da integração do formulário com o Drive

---
+++ editor-saymon-cesar/form-config.js
@@ -0,0 +1,7 @@
+// Integração do formulário editorial com o Google Apps Script.
+// Depois de implantar o Apps Script, cole a URL terminada em /exec entre as aspas.
+window.EDITORIAL_FORM_CONFIG = Object.freeze({
+  appsScriptUrl: '',
+  maxFileSizeMb: 10,
+  acceptedExtensions: ['pdf', 'doc', 'docx', 'odt', 'rtf']
+});
