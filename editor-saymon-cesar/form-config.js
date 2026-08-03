6e1b3fb845864947096bed263d8ccdb01cc3d692
fantasticoseditora <fantasticoseditora@gmail.com>
2026-08-03 19:43:06 -0300

    Ativa envio do formulário editorial para o Google Drive

--- editor-saymon-cesar/form-config.js
+++ editor-saymon-cesar/form-config.js
@@ -1,7 +1,6 @@
 // Integração do formulário editorial com o Google Apps Script.
-// Depois de implantar o Apps Script, cole a URL terminada em /exec entre as aspas.
 window.EDITORIAL_FORM_CONFIG = Object.freeze({
-  appsScriptUrl: '',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbz5gOy4fxhf-_FEfZAo7OMms_fE6ZGs1OmGEdB5bPAWCZCZY29rvmqHeYL0cn2U29uR/exec',
   maxFileSizeMb: 10,
   acceptedExtensions: ['pdf', 'doc', 'docx', 'odt', 'rtf']
 });
