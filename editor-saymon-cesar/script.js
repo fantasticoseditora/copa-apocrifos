21072495fca1f4a53071980b00cd922144af2adb
fantasticoseditora <fantasticoseditora@gmail.com>
2026-08-04 01:25:46 -0300

    Força atualização do carregamento da arte mobile

--- editor-saymon-cesar/script.js
+++ editor-saymon-cesar/script.js
@@ -1,5 +1,5 @@
 (() => {
-  import('./hero-image.js?v=20260804e').catch(() => {});
+  import('./hero-image.js?v=20260804f').catch(() => {});
 
   const menuButton = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.main-nav');
